532f24959e812a25acb2bc7b2de5b3612deceb47
ohmkrishnasarang <njatuvettysaru@gmail.com>
2021-11-25 15:20:41 +0530

    first commit

---
+++ grade.py
@@ -0,0 +1,26 @@
+a=int(input("mark in maths"))
+b=int(input("mark in english"))
+c=int(input("mark in malayalam"))
+d=int(input("mark in hindi"))
+e=int(input("mark in physics"))
+sum=a+b+c+d+e
+avg=(sum/5)
+print("sum",sum)
+print("average",avg)
+if avg>80:
+ print("A grade")
+elif avg>60:
+ print("B grade")
+elif avg>40:
+ print("B grade")
+else:
+ print("failed")
+
+
+
+
+
+
+
+
+
